88948390f64c426710d6303b460c84e7cbde71e3
zizan-azman <azizan.azman.fe@gmail.com>
2025-12-30 16:46:39 +0800

    updated content wording

--- src/app/mock/display.mock.ts
+++ src/app/mock/display.mock.ts
@@ -3,5 +3,6 @@ import { DisplayConfig } from '../models/display/display.model';
 export const MockDisplay: DisplayConfig = {
   name: 'Azizan Azman',
   role: 'Front End Developer',
-  summary: 'I build accessible, pixel-perfect digital experiences for the web.',
+  summary:
+    'I love to build pixel-perfect UI with scalable Design System for the web.',
 };
