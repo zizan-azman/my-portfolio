00ed0255264a02966b749bd51bda843d5253ebdb
zizan-azman <azizan.azman.fe@gmail.com>
2026-01-26 13:18:14 +0800

    Fix typo

--- src/app/mock/display.mock.ts
+++ src/app/mock/display.mock.ts
@@ -2,7 +2,7 @@ import { DisplayConfig } from '../models/display/display.model';
 
 export const MockDisplay: DisplayConfig = {
   name: 'Azizan Azman',
-  role: 'Front End Developer',
+  role: 'Front-End Developer',
   summary:
     'I love to build pixel-perfect UI with scalable Design System for the web.',
 };
--- src/app/mock/experience.mock.ts
+++ src/app/mock/experience.mock.ts
@@ -4,7 +4,7 @@ export const MockExperience: ExperienceConfig = {
   experiences: [
     {
       yos: 'JUN 2023 — NOV 2025',
-      text: 'Front End Developer · Orsted Services MY Sdn Bhd',
+      text: 'Front-End Developer · Orsted Services MY Sdn Bhd',
       href: 'https://orsted.my/malaysia-office',
       paragraph: `Built and maintained <span class="text-emphasize-light">50+ shared UI components</span> that power <span class="text-emphasize-light">Ørsted's 60+ public-facing websites</span>. Collaborated with <span class="text-emphasize-light">cross-functional teams</span> within the <span class="text-emphasize-light">Agile Release Train (ART)</span> to deliver high-quality experiences for both <span class="text-emphasize-light">site visitors</span> and <span class="text-emphasize-light">Sitecore content editors</span>. Enhanced the design system through <span class="text-emphasize-light">Design Tokens</span> and improved SEO performance by optimizing <span class="text-emphasize-light">Core Web Vitals</span>.`,
       tags: [
@@ -23,7 +23,7 @@ export const MockExperience: ExperienceConfig = {
       ],
     },
     {
-      yos: 'JAN 2019 — May 2023',
+      yos: 'APR 2019 — MAY 2023',
       text: 'System Engineer · Silverline Dynamics Sdn Bhd',
       href: 'https://www.silverlinedynamics.com.my/company/overview/',
       paragraph: `Built and maintained <span class="text-emphasize-light">front-end applications</span> across <span class="text-emphasize-light">multiple client projects</span>, including a <span class="text-emphasize-light">cloud-based POS system</span> and a <span class="text-emphasize-light">highway-management system</span>. Introduced a <span class="text-emphasize-light">monorepo architecture</span> using <span class="text-emphasize-light">Nx</span> and <span class="text-emphasize-light">Angular</span> to streamline development and improve consistency across codebases. <span class="text-emphasize-light">Mentored two developers</span> to elevate <span class="text-emphasize-light">code quality</span>. Collaborated with <span class="text-emphasize-light">designers</span> to deliver clear, scalable <span class="text-emphasize-light">UI/UX</span> using <span class="text-emphasize-light">Figma</span> and <span class="text-emphasize-light">Atomic Design</span> principles.`,
